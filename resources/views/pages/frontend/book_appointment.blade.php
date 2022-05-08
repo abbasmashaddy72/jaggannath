724546a755980524ec0db186a6b37d0a02f8daf2
Syed Kounain Abbas Rizvi <abbasmashaddy72@gmail.com>
2022-05-09 03:26:08 +0530

    Almost Done Pending Frontend - Gallery, Home; Backend - Department, Insurance, Service, Slider, Team

--- resources/views/pages/frontend/book_appointment.blade.php
+++ resources/views/pages/frontend/book_appointment.blade.php
@@ -10,38 +10,6 @@
                 <div class="shadow-testimonial rounded-lg border-gray-200 border-2 py-10 px-8 md:p-[60px] lg:p-10 2xl:p-[60px] sm:py-12 sm:px-10 lg:py-12 lg:px-10 wow fadeInUp"
                     data-wow-delay=".2s">
                     @livewire('form.frontend.book-appointment-form')
-                    {{-- <form>
-                        <div class="mb-6">
-                            <label for="fullName" class="block text-xs text-dark">Full Name<span
-                                    class="text-red-600 ml-2">*</span></label>
-                            <input type="text" name="fullName" placeholder="Adam Gelius"
-                                class="w-full border-0 border-b border-[#f1f1f1] focus:border-primary focus:outline-none py-4" />
-                        </div>
-                        <div class="mb-6">
-                            <label for="email" class="block text-xs text-dark">Email<span
-                                    class="text-red-600 ml-2">*</span></label>
-                            <input type="email" name="email" placeholder="example@yourmail.com"
-                                class="w-full border-0 border-b border-[#f1f1f1] focus:border-primary focus:outline-none py-4" />
-                        </div>
-                        <div class="mb-6">
-                            <label for="phone" class="block text-xs text-dark">Phone<span
-                                    class="text-red-600 ml-2">*</span></label>
-                            <input type="text" name="phone" placeholder="+885 1254 5211 552"
-                                class="w-full border-0 border-b border-[#f1f1f1] focus:border-primary focus:outline-none py-4" />
-                        </div>
-                        <div class="mb-6">
-                            <label for="message" class="block text-xs text-dark">Message<span
-                                    class="text-red-600 ml-2">*</span></label>
-                            <textarea name="message" rows="1" placeholder="type your message here"
-                                class="w-full border-0 border-b border-[#f1f1f1] focus:border-primary focus:outline-none py-4  resize-none"></textarea>
-                        </div>
-                        <div class="mb-0">
-                            <button type="submit"
-                                class="inline-flex items-center justify-center py-4 px-6 rounded text-white bg-primary text-base font-medium hover:bg-dark transition duration-300 ease-in-out">
-                                Send Message
-                            </button>
-                        </div>
-                    </form> --}}
                 </div>
             </div>
         </div>
